c4f0b4d4e1792f850234d20ced93fa61bd1cda90
MacKaris <36438269+MacKaris@users.noreply.github.com>
2018-02-26 18:42:16 +0200

    Update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -6,21 +6,23 @@ from flask import Flask
 application = Flask(__name__)
 
 @application.route("/")
+
+print ("on tamakin saatana tyomaa")
 def hello():    
 	print ("spede")
-	return "Hello Wooiorld! Greetings from "+socket.gethostname()+"\n"
-	return "Rock rock! \n"    
+	#return "Hello Wooiorld! Greetings from "+socket.gethostname()+"\n"
+	#return "Rock rock! \n"    
 
 	FILE_NAME = "log"
 	if (os.path.isfile(FILE_NAME)):
 		with open('log', 'a') as f:
 			aika=time.time()
-			f.write("kullikulli, "+str(aika)+'\n')
+			f.write(socket.gethostname()+" : "+str(aika)+'\n')
 		f.closed
 	else:
 		with open('log', 'w') as f:
 			aika=time.time()
-			f.write("kullikulli, "+str(aika)+'\n')
+			f.write(socket.gethostname()+" : "+str(aika)+'\n')
 		f.closed
 
 	with open('log', 'r') as f:
